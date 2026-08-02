839d25e542c6cad600a2034bffccfb2d1aa4e32d
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-08-02 19:37:27 +0900

    Add 의상.java
    Implement solution to count outfit combinations based on clothing types.

---
+++ week01/Jihye/의상.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map =new HashMap<>();
+        for(int i =0; i<clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
+        }
+        List<String> key = new ArrayList<>(map.keySet());
+        for(String k : key){
+            int n = map.get(k);
+            answer *= (n+1);
+        }
+        
+        return answer-1;
+    }
+}

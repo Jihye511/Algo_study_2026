426a3bca6dc91f4a0fbd8fe8e700057ed4e54294
chanki00 <rlacksrl00@naver.com>
2026-08-09 12:16:03 +0900

    week 02

---
+++ week02/chanki/02_같은_숫자는_싫어.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+    public List<Integer> solution(int []arr) {
+        List<Integer> answer = new ArrayList<>();
+
+        int prev = -1;
+        for (int a : arr) {
+            if (a != prev) {
+                answer.add(a);
+            }
+            prev = a;
+        }
+
+
+        return answer;
+    }
+}

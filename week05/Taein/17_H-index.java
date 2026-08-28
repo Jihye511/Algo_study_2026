f542577f6655c4895a1d7a2799cc240a3192811b
TaeIn <81225723+Intae0099@users.noreply.github.com>
2026-08-28 19:16:49 +0900

    week05 H-Index

---
+++ week05/Taein/17_H-index.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int N = citations.length;
+        for(int i = 0; i < N; i++){
+            if(citations[i] >= N - i && i < citations[i]) return N - i;
+        }
+        return 0;
+    }
+}
